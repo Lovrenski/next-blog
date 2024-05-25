8cba82329afa08473696f3d6ce85e83a5bb32489
Lovrenski <lavodenicoste@gmail.com>
2024-05-25 20:54:28 +0700

    Adding Featured, Card List, and Menu

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import styles from "./card.module.css";
+import Link from "next/link";
+
+export const Card = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image src="/p1.jpeg" alt="post" fill className={styles.image} />
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.detail}>
+          <span className={styles.date}>11.02.2023 - </span>
+          <span className={styles.category}>CULTURE</span>
+        </div>
+        <h1 className={styles.title}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+          dapibus orci sit amet est rhoncus mollis vitae a nisl. Nulla a
+          faucibus ligula. Aliquam fringilla malesuada neque, et viverra purus
+          rhoncus quis. Sed sed rhoncus nisi. Nunc pellentesque, metus a
+          venenatis varius, nisl risus malesuada massa, at sollicitudin nunc
+          magna a tellus.
+        </p>
+        <Link href="/" className={styles.link}>
+          Read more
+        </Link>
+      </div>
+    </div>
+  );
+};
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,8 +1,35 @@
 import React from "react";
 import styles from "./featured.module.css";
+import Image from "next/image";
 
 const Featured = () => {
-  return <div className={styles.container}>Featured</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, white dev here!</b> Discover my stories and creative ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="yessir" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+            dapibus orci sit amet est rhoncus mollis vitae a nisl.
+          </h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+            dapibus orci sit amet est rhoncus mollis vitae a nisl. Nulla a
+            faucibus ligula. Aliquam fringilla malesuada neque, et viverra purus
+            rhoncus quis. Sed sed rhoncus nisi. Nunc pellentesque, metus a
+            venenatis varius, nisl risus malesuada massa, at sollicitudin nunc
+            magna a tellus.
+          </p>
+          <button className={styles.button}>Read</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
